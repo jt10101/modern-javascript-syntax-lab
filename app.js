bd425bf0354eec11f5abba31460a8fd6f3a095ec
Jacob Tham <jacobtham3@gmail.com>
2025-05-21 17:04:50 +0800

    ex1to6

--- app.js
+++ app.js
@@ -12,3 +12,107 @@
 const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 
 // Your code here:
+const numsTimesTwo = [];
+
+nums.map((x) => {
+  numsTimesTwo.push(x * 2);
+});
+
+console.log(numsTimesTwo);
+
+// ! Exercise 2:
+// a. Given the provided `pizzaToppings` array, use destructuring to extract
+//    the first and second values and place them into variables. Name the
+//    variable that corresponds to the first value `firstIngredient`. Name the
+//    variable that corresponds to the second value `secondIngredient`.
+//
+// b. Console log the `firstIngredient` and `secondIngredient` variables.
+//
+// Starting code (don't modify this):
+
+const pizzaToppings = ["Pineapple", "Olives", "Anchovies"];
+
+// Your code here:
+
+let firstIngredient = pizzaToppings[0];
+let secondIngredient = pizzaToppings[1];
+
+console.log(firstIngredient, secondIngredient);
+
+// ! Exercise 3:
+// a. Given the provided `car` object, use destructuring to create two
+//    variables: `make` and `model` that will hold the respective values.
+//
+// b. Console log the `make` and `model` variables.
+//
+// Starting code (don't modify this):
+
+const car = {
+  make: "Audi",
+  model: "Q5",
+};
+
+// Your code here:
+let make = car.make;
+let model = car.model;
+
+console.log(make, model);
+
+// ! Exercise 4:
+// a. Duplicate the provided `morePizzaToppings` array using the spread
+//    operator and assign it to a variable named `uncontroversialPizzaToppings`.
+//
+// b. Console log the `uncontroversialPizzaToppings` variable.
+//
+// Starting code (don't modify this):
+
+const morePizzaToppings = ["Cheese", "Sauce"];
+
+// Your code here:
+const uncontroversialPizzaToppings = [...morePizzaToppings];
+
+console.log(uncontroversialPizzaToppings);
+
+// ! Exercise 5:
+// a. Duplicate the provided `anotherCar` object and spread its values into a
+//    new variable named `myCar`.
+//
+// b. Change the `make` and `model` properties of the `myCar` object to new
+//    values.
+//
+// c. Console log both objects and observe the results.
+//
+// Starting code (don't modify this):
+
+const anotherCar = {
+  make: "Toyota",
+  model: "RAV4",
+};
+
+// Your code here:
+
+let myCar = { ...anotherCar };
+myCar.make = "BMW";
+myCar.model = "X5";
+
+console.log(myCar, anotherCar);
+
+// ! Exercise 6:
+// a. Define a variable named `propertyName` and assign a string (like
+//    'username', 'age', or 'email') to it.
+//
+// b. Create an object named `userProfile`.
+//
+// c. Use `propertyName` as a dynamic key in `userProfile`. Assign it a
+//    relevant value.
+//
+// d. Console log the `userProfile` object to see the result.
+//
+// Your code here:
+
+let propertyName = "username";
+let userProfile = {};
+
+userProfile[propertyName] = "Jacob Tham";
+
+console.log(userProfile);
